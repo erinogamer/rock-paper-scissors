49f73d218207b952f3c2726cfdfbf9b46ff69d71
erinogamer <windows12videos@gmail.com>
2025-02-03 14:39:45 +0100

    made a game, free use

--- main.ts
+++ main.ts
@@ -1,6 +1,31 @@
 let ruka = 0
 input.onGesture(Gesture.Shake, function () {
     ruka = randint(1, 3)
+    if (ruka == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (ruka == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
 })
 basic.forever(function () {
 	
